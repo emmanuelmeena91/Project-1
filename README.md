76a2e80fd70f73ffe8abe2f8cf5ca3c458926365
emmanuelmeena91 <emmanuel.meena@moringaschool.com>
2023-07-08 20:34:24 +0300

    Project complete

--- README.md
+++ README.md
@@ -1,1 +1,23 @@
-# Project-1
+# Project Name
+To-Do-List
+
+This is a simple to-do list app that allows users to add, edit, and delete tasks. Users can enter their name to personalize the app, and the app stores the tasks in the browser's local storage so that they persist across sessions.
+# Usage
+To use the app, enter your name in the input field and click the "Add name" button. Once you've added your name, the app will display a personalized welcome message and an input field to add tasks. To add a task, simply enter the task in the input field and click the "Add task" button. To delete a task, click the "Delete" button next to the task. To edit your name, click the "Edit name" button.
+
+The app stores the tasks in the browser's local storage, so they will persist across sessions. If you close the app and open it again, your tasks will still be there.
+
+
+# Contributing
+Contributions are welcome! If you find a bug or have a feature request, please open an issue or submit a pull request. To set up a development environment, simply download the files and open them in your favorite text editor. You can then make changes and test them in your browser.
+
+## Technologies Used
+
+- JavaScript
+- HTML
+- CSS
+# Installation
+To install the app, simply download the HTML, CSS, and JavaScript files and open the HTML file in your web browser. The app does not require any additional dependencies.
+
+# License
+This project is licensed under the [MIT License](LICENSE).
---
+++ script.js
@@ -0,0 +1,75 @@
+// Get DOM elements
+const nameInput = document.getElementById('name-input');
+const addNameBtn = document.getElementById('add-name-btn');
+const nameSection = document.getElementById('name-section');
+const studentName = document.getElementById('student-name');
+const editNameBtn = document.getElementById('edit-name-btn');
+const taskInputSection = document.getElementById('task-input-section');
+const taskInput = document.getElementById('task-input');
+const addTaskBtn = document.getElementById('add-task-btn');
+const taskList = document.getElementById('task-list');
+
+// State variables
+let name = '';
+let tasks = [];
+
+// Event listeners
+addNameBtn.addEventListener('click', addName);
+editNameBtn.addEventListener('click', editName);
+addTaskBtn.addEventListener('click', addTask);
+
+// Functions
+function addName() {
+  name = nameInput.value.trim();
+  if (name) {
+    studentName.textContent = name;
+    nameInput.value = '';
+    nameInput.disabled = true;
+    addNameBtn.disabled = true;
+    nameSection.classList.remove('hidden');
+    taskInputSection.classList.remove('hidden');
+  }
+}
+
+function editName() {
+  nameInput.value = name;
+  nameInput.disabled = false;
+  addNameBtn.disabled = false;
+  nameSection.classList.add('hidden');
+  taskInputSection.classList.add('hidden');
+  tasks = [];
+  renderTasks();
+}
+
+function addTask() {
+  const task = taskInput.value.trim();
+  if (task) {
+    tasks.push(task);
+    taskInput.value = '';
+    renderTasks();
+  }
+}
+
+function deleteTask(index) {
+  tasks.splice(index, 1);
+  renderTasks();
+}
+
+function renderTasks() {
+  taskList.innerHTML = '';
+  tasks.forEach((task, index) => {
+    const taskItem = document.createElement('div');
+    taskItem.classList.add('task-item');
+
+    const taskText = document.createElement('span');
+    taskText.textContent = task;
+    taskItem.appendChild(taskText);
+
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete';
+    deleteBtn.addEventListener('click', () => deleteTask(index));
+    taskItem.appendChild(deleteBtn);
+
+    taskList.appendChild(taskItem);
+  });
+}
